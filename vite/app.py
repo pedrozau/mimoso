49dc3d065c59c07d48bcd03b79a29e0aaa32a5be
pedrozau <pedrozau28@gmail>
2022-08-02 20:38:09 +0100

    alterações novas

--- vite/app.py
+++ vite/app.py
@@ -14,7 +14,5 @@ def create_app(**config):
     configuration.load_extensions(app)
     return app
 
-app = create_app()
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=80)
+
 #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1python1@localhost/mimoso"
