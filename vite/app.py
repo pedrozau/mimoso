34dda0308c3bb008741945040f5404b9d5074803
Pedro Zau <57961751+pedrozau@users.noreply.github.com>
2022-08-15 15:06:26 +0100

    Update app.py
    Signed-off-by: Pedro Zau <57961751+pedrozau@users.noreply.github.com>

--- vite/app.py
+++ vite/app.py
@@ -14,6 +14,9 @@ def create_app(**config):
     
     app = minimal_app(**config)
     configuration.load_extensions(app)
+    if __name__ == "__main__":
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=8080)
     return app
 
 
