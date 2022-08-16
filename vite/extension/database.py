41e08a0d69f858a7a3a8c6e064f49f2f1bc4fb75
Pedro Zau <57961751+pedrozau@users.noreply.github.com>
2022-08-16 13:27:53 +0100

    alteration in extension database

--- vite/extension/database.py
+++ vite/extension/database.py
@@ -6,8 +6,7 @@ db = SQLAlchemy()
 def init_app(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    
 
 
 
