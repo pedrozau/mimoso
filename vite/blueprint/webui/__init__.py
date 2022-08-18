251c139af33afa89c1a580fc1b34ddb1861df384
Pedro Zau <57961751+pedrozau@users.noreply.github.com>
2022-08-18 14:37:25 +0100

    New altaration  in index, auth, view

--- vite/blueprint/webui/__init__.py
+++ vite/blueprint/webui/__init__.py
@@ -1,11 +1,12 @@
-from flask import Blueprint 
+from flask import Blueprint
+from vite.blueprint.webui.view import auth 
 from .view import index 
 
 
 bp = Blueprint("webui",__name__,template_folder="templates")
 
 bp.add_url_rule("/", view_func=index)
-
+bp.add_url_rule("/auth",view_func=auth)
 
 def init_app(app):
     app.register_blueprint(bp)
--- vite/blueprint/webui/view.py
+++ vite/blueprint/webui/view.py
@@ -1,7 +1,12 @@
 from flask import Flask,render_template
-app = Flask(__name__) 
+
 
 def index():
     return render_template("index.html")
+
+
+def auth():
+    
+    return "Hello"
 
 
